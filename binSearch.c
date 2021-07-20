e8d6293a14e20b5ce60853a0d5b88f6841aa05fa
Elliot Lichtenberg <74284637+WindInstruments94@users.noreply.github.com>
2021-07-20 16:50:22 -0500

    Create binSearch.c

---
+++ binSearch.c
@@ -0,0 +1,43 @@
+/* binary search of a sorted array
+
+NOTE:
+Arrays in C are passed to functions as pointers and do not
+retain their length – a length parameter is passed in along
+with the array. */
+
+// #include <stdbool.h>
+// #include <string.h>
+
+bool binarySearch(int array[], int length, int target)
+{
+    if (length == 1 && array[0] != target)
+    {
+        return false;
+    }
+
+    int start = 0;
+    int end = length;
+    int middle = length / 2;
+
+    if (target == array[middle])
+    {
+        return true;
+    }
+    else if (target < array[middle])
+    {
+        int newLength = middle;
+        int newArray[newLength];
+        memcpy(newArray, array, sizeof(array[0]) * newLength);
+        return binarySearch(newArray, newLength, target);
+    }
+    else if (target > array[middle])
+    {
+        int newLength = end - middle;
+        int newArray[newLength];
+        memcpy(newArray, &array[middle], sizeof(array[0]) * newLength);
+        return binarySearch(newArray, newLength, target);
+    }
+  
+    // to stop compiler error messages
+    return false;
+}
