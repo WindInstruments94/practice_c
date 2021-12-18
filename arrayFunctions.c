77bc7377bb76f227489e113bb845af1583e26bed
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-12-18 14:52:29 -0600

    Rename functions

--- arrayFunctions.c
+++ arrayFunctions.c
@@ -5,7 +5,7 @@ Arrays in C are passed to functions as pointers and do not
 retain their length – a length parameter is passed in along
 with the array. */
 
-int getLargestElement(int array[], int length)
+int max(int array[], int length)
 {
     int largest = array[0];
     for (int i = 0; i < length; i++)
@@ -18,7 +18,7 @@ int getLargestElement(int array[], int length)
     return largest;
 }
 
-int getSmallestElement(int array[], int length)
+int min(int array[], int length)
 {
     int smallest = array[0];
     for (int i = 0; i < length; i++)
@@ -31,7 +31,7 @@ int getSmallestElement(int array[], int length)
     return smallest;
 }
 
-float getAverageOfArray(int array[], int length)
+float average(int array[], int length)
 {
     int sum = 0;
     for (int i = 0; i < length; i++)
